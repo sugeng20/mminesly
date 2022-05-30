d3b41cb407b2ffd1e6eeea61f96bbc70dcee4e75
sugeng20 <sugeng.wanantara@gmail.com>
2022-05-30 15:20:18 +0700

    navigasi

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,14 @@
     <link
         href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,400;0,600;0,700;0,800;0,900;1,400;1,600;1,700;1,900&display=swap"
         rel="stylesheet">
+    <style>
+        @font-face {
+            font-family: 'BubbleWater';
+            /*memberikan nama bebas untuk font*/
+            src: url('fonts/PoetsenOne-Regular.ttf');
+            /*memanggil file font eksternalnya di folder nexa*/
+        }
+    </style>
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 
--- resources/views/pages/siswa/kompetensi-dasar.blade.php
+++ resources/views/pages/siswa/kompetensi-dasar.blade.php
@@ -8,7 +8,7 @@ Kompetensi Dasar
 <div class="pl-6 py-4 rounded-lg h-[68vh] bg-white shadow-lg w-full relative">
     <div
         class="w-80 h-16 absolute -top-7 left-1/2 -translate-x-1/2 bg-cover bg-center flex justify-center items-center bg-[#F2BA70] rounded-full shadow-lg">
-        <span class=" font-bold text-2xl text-[#622E2A]">Kompetensi Dasar</span>
+        <span class="text-3xl text-[#622E2A]" style="font-family: 'BubbleWater'">Kompetensi Dasar</span>
     </div>
 
     <div class="w-full flex flex-wrap overflow-hidden overflow-y-scroll mt-10">
@@ -21,6 +21,17 @@ Kompetensi Dasar
         <div class="w-full lg:w-1/2 px-6">
             <img src="{{ asset('img/kompetensi-dasar.png') }}" width="350" alt="">
         </div>
+
+    </div>
+
+    <div
+        class="w-40 h-12 absolute -bottom-6 left-1/2 -translate-x-1/2 bg-cover bg-center flex justify-between items-center bg-[#F2BA70] rounded-full shadow-xl">
+        <a href="#" class="hover:scale-125 duration-500 ease-in-out">
+            <img src="{{ asset('img/ic_prev.png') }}" alt="" width="60">
+        </a>
+        <a href="#" class="hover:scale-125 duration-500 ease-in-out">
+            <img src="{{ asset('img/ic_next.png') }}" alt="" width="60">
+        </a>
     </div>
 
 </div>
