ba7242eb5f3d1725d7cc063397147d83db3e73c0
sugeng20 <sugeng.wanantara@gmail.com>
2022-06-03 11:13:55 +0700

    peta konsep

--- resources/views/pages/siswa/peta-konsep.blade.php
+++ resources/views/pages/siswa/peta-konsep.blade.php
@@ -5,7 +5,7 @@ Peta Konsep
 @endsection
 
 @section('content')
-<div class="pl-3 lg:pl-8 py-4 rounded-lg h-[65vh] bg-white shadow-lg w-full relative">
+<div class="pl-4  py-4 rounded-lg h-[65vh] bg-white shadow-lg w-full relative">
     <div
         class="lg:w-96 w-72 h-16 absolute -top-7 left-1/2 -translate-x-1/2 bg-cover bg-center flex justify-center items-center bg-[#F2BA70] rounded-full shadow-lg">
         <span class="lg:text-3xl text-2xl text-[#622E2A] uppercase" style="font-family: 'BubbleWater'">Peta
@@ -13,8 +13,8 @@ Peta Konsep
     </div>
 
     <div class="overflow-y-scroll mt-8 relative flex flex-wrap h-[55vh] overflow-hidden">
-        <div class="w-full px-8 mb-10">
-            <img src="{{ asset('img/indikator.png') }}" width="400" height="300" alt="" class="float-right ml-4">
+        <div class="w-full mb-10 justify-center  flex">
+            <img src="{{ asset('img/peta_konsep.png') }}" width="300" alt="">
         </div>
 
 
