c57c4cdf12bc3ce07534f5fda4b4e66bfaaa174e
sugeng20 <sugeng.wanantara@gmail.com>
2022-06-03 22:06:37 +0700

    materi pembahasan

--- resources/views/pages/siswa/materi-pembahasan.blade.php
+++ resources/views/pages/siswa/materi-pembahasan.blade.php
@@ -5,19 +5,96 @@ Materi Pembahasan
 @endsection
 
 @section('content')
-<div class="pl-3 lg:pl-8 py-4 rounded-lg h-[65vh] bg-white shadow-lg w-full relative">
+<div class="py-4 rounded-lg h-[65vh] bg-white shadow-lg w-full relative">
     <div
         class="lg:w-96 w-72 h-16 absolute -top-7 left-1/2 -translate-x-1/2 bg-cover bg-center flex justify-center items-center bg-[#F2BA70] rounded-full shadow-lg">
         <span class="lg:text-3xl text-2xl text-[#622E2A] uppercase" style="font-family: 'BubbleWater'">Materi
             Pembahasan</span>
     </div>
 
-    <div class="overflow-y-scroll mt-8 relative flex flex-wrap h-[55vh] overflow-hidden">
-        <div class="w-full px-8 mb-10">
-            <img src="{{ asset('img/indikator.png') }}" width="400" height="300" alt="" class="float-right ml-4">
+    <div class="overflow-y-scroll mt-8 px-4 relative flex flex-wrap h-[55vh] overflow-hidden">
+        <div class="w-full flex justify-center flex-wrap mt-4 mb-10">
+            <div class="w-full lg:w-1/3 px-4 mb-8 justify-center flex">
+                <a href="#" class="hover:scale-105 transition duration-500">
+                    <div class="bg-[#E9F5F9] shadow-lg h-80 overflow-hidden rounded-3xl">
+                        <div class="h-14 bg-[#31647A] items-center justify-center flex">
+                            <span class="font-bold text-2xl text-white">SUB MATERI 1</span>
+                        </div>
+                        <div class="flex justify-center">
+                            <img src="{{ asset('img/materi/sub_materi_1.png') }}" class="w-60 mt-6 px-4" alt="">
+                        </div>
+                        <p class="text-center px-2 font-bubbleWater mt-6">
+                            Kondisi Geografis Benua Asia dan Benua Lainnya
+                        </p>
+                    </div>
+                </a>
+            </div>
+
+            <div class="w-full lg:w-1/3 px-4 mb-8 justify-center flex">
+                <a href="#" class="hover:scale-105 transition duration-500">
+                    <div class="bg-[#E9F5F9] shadow-lg h-80 overflow-hidden rounded-3xl">
+                        <div class="h-14 bg-[#31647A] items-center justify-center flex">
+                            <span class="font-bold text-2xl text-white">SUB MATERI 2</span>
+                        </div>
+                        <div class="flex justify-center">
+                            <img src="{{ asset('img/materi/sub_materi_2.png') }}" class="w-60 mt-6 px-4" alt="">
+                        </div>
+                        <p class="text-center px-2 font-bubbleWater mt-6">
+                            Potensi Sumber Daya Alam di Benua Asia dan Benua Lainnya
+                        </p>
+                    </div>
+                </a>
+            </div>
+
+            <div class="w-full lg:w-1/3 px-4 mb-8 justify-center flex">
+                <a href="#" class="hover:scale-105 transition duration-500">
+                    <div class="bg-[#E9F5F9] shadow-lg h-80 overflow-hidden rounded-3xl">
+                        <div class="h-14 bg-[#31647A] items-center justify-center flex">
+                            <span class="font-bold text-2xl text-white">SUB MATERI 3</span>
+                        </div>
+                        <div class="flex justify-center">
+                            <img src="{{ asset('img/materi/sub_materi_3.png') }}" class="w-60 mt-6 px-4" alt="">
+                        </div>
+                        <p class="text-center px-2 font-bubbleWater mt-6">
+                            Sumber Daya Manusia di Benua Asia dan Benua Lainnya
+                        </p>
+                    </div>
+                </a>
+            </div>
+
+            <div class="w-full lg:w-1/3 px-4 mb-8 justify-center flex">
+                <a href="#" class="hover:scale-105 transition duration-500">
+                    <div class="bg-[#E9F5F9] shadow-lg h-80 overflow-hidden rounded-3xl">
+                        <div class="h-14 bg-[#31647A] items-center justify-center flex">
+                            <span class="font-bold text-2xl text-white">SUB MATERI 4</span>
+                        </div>
+                        <div class="flex justify-center">
+                            <img src="{{ asset('img/materi/sub_materi_1.png') }}" class="w-60 mt-6 px-4" alt="">
+                        </div>
+                        <p class="text-center px-2 font-bubbleWater mt-6">
+                            Distribusi Potensi Wilayah Benua Asia dan Benua Lainnya
+                        </p>
+                    </div>
+                </a>
+            </div>
+
+            <div class="w-full lg:w-1/3 px-4 mb-8 justify-center flex">
+                <a href="#" class="hover:scale-105 transition duration-500">
+                    <div class="bg-[#E9F5F9] shadow-lg h-80 overflow-hidden rounded-3xl">
+                        <div class="h-14 bg-[#31647A] items-center justify-center flex">
+                            <span class="font-bold text-2xl text-white">SUB MATERI 5</span>
+                        </div>
+                        <div class="flex justify-center">
+                            <img src="{{ asset('img/materi/sub_materi_1.png') }}" class="w-60 mt-6 px-4" alt="">
+                        </div>
+                        <p class="text-center px-2 font-bubbleWater mt-6">
+                            Dampak interaksi antarruang Asia dan Benua Lainnya
+                        </p>
+                    </div>
+                </a>
+            </div>
+
         </div>
-
-
     </div>
 
     <div
